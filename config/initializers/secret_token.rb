ac0ecc01d0bfd26e78a1c014a0c0d497ae895bc2
Piotr Steininger <piotr.steininger@gmail.com>
2013-05-16 15:40:27 +0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gohike::Application.config.secret_token = '564a59c3d66dc589c9eb535236be6f6eff2718191c1c18603c328b4adabe950ffa6d6f9debb4a3d753ba5aa67d667108413ca8b333a5b177639676363166ea2c'
