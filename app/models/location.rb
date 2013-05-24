1cc70feb388646d8d171257056b71da9669da918
Piotr Steininger <piotr.steininger@gmail.com>
2013-05-24 13:13:10 +0200

    fix for geocoding issue

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,7 +2,7 @@ class Location < ActiveRecord::Base
   acts_as_gmappable  :process_geocoding => :geocode?
   attr_accessible :address, :city, :description_en, :description_nl, :latitude, :longitude, :name_en, :name_nl, :postal_code
   def geocode?
-    (!address.blank? && (lat.blank? || lng.blank?)) || address_changed?
+    (!address.blank? && (latitude.blank? || longitude.blank?))
   end
 
   def gmaps4rails_address
