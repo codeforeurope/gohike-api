055426d85ef354965b466213c32b5e30e8ee62a4
Piotr Steininger <piotr.steininger@gmail.com>
2013-05-30 13:38:06 +0200

    Minor fix to handle update and crop validations

--- app/models/location.rb
+++ app/models/location.rb
@@ -35,7 +35,12 @@ class Location < ActiveRecord::Base
   MIN_HEIGHT = 380
 
   def validate_minimum_image_size
-    img = MiniMagick::Image.open(image_url)
+    if image.file.respond_to?(:sanitize_regexp)
+      img = MiniMagick::Image.read(image.file)
+    else
+
+      img = MiniMagick::Image.open(image.url)
+    end
     unless img[:width] >= MIN_WIDTH && img[:height] >= MIN_HEIGHT
       errors.add :image, :image_minimum_size, :min_width => MIN_WIDTH, :min_height => MIN_HEIGHT
     end
