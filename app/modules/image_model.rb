92d0045d5109b44af4173ec47071cc814e9035be
Piotr Steininger <piotr.steininger@gmail.com>
2013-06-03 15:25:36 +0200

    Fixed 500 Error when adding a location w/o image

--- app/modules/image_model.rb
+++ app/modules/image_model.rb
@@ -13,14 +13,16 @@ module ImageModel
   MIN_HEIGHT = 380
 
   def validate_minimum_image_size
-    if image.file.respond_to?(:sanitize_regexp)
-      img = MiniMagick::Image.read(image.file)
-    else
+    unless image.blank?
+      if image.file.respond_to?(:sanitize_regexp)
+        img = MiniMagick::Image.read(image.file)
+      else
 
-      img = MiniMagick::Image.open(image.url)
-    end
-    unless img[:width] >= MIN_WIDTH && img[:height] >= MIN_HEIGHT
-      errors.add :image, :image_minimum_size, :min_width => MIN_WIDTH, :min_height => MIN_HEIGHT
+        img = MiniMagick::Image.open(image.url)
+      end
+      unless img[:width] >= MIN_WIDTH && img[:height] >= MIN_HEIGHT
+        errors.add :image, :image_minimum_size, :min_width => MIN_WIDTH, :min_height => MIN_HEIGHT
+      end
     end
   end
 end
