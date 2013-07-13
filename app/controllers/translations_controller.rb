b03978bb5d4d083f54f6ee49e897bb44b268932d
Piotr Steininger <piotr.steininger@gmail.com>
2013-07-13 22:19:17 +0200

    handling of nested forms

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -1,7 +1,16 @@
 class TranslationsController < ApplicationController
   def new
+
+    @form_source = []
+
     resource_type = params[:resource_type].constantize
+
     @resource = params[:resource_id] == 0.to_s ? resource_type.new : resource_type.find(params[:resource_id])
+    if resource_type == Route
+      @parent_resource = @resource.route_profile
+      @form_source << @parent_resource
+    end
+    @form_source << @resource
     @locale = params[:target_locale]
 
     respond_to do |format|
