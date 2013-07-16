4913aec57add4489e7d8a3de8422a5ecc3d9d27f
Piotr Steininger <piotr.steininger@gmail.com>
2013-07-16 15:09:53 +0200

    Added logic to set locale based on a fall-back strategy: 1. Try from user's locale 2. Try from browser's locale 3. Explicitly defined in URL (trumps all other ones)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,24 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    if params[:locale].blank?
+      unless current_user.nil? || current_user.locale.nil?
+        I18n.locale = current_user.locale
+      else
+        I18n.locale = extract_locale_from_header
+      end
+
+    else
+      I18n.locale = params[:locale]
+    end
+  end
+
+  def default_url_options(options = {})
+    {locale: I18n.locale}
+  end
+
+  private
+  def extract_locale_from_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first || I18n.default_locale
   end
 end
