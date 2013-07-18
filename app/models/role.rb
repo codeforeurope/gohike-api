64464895d7f6041a89e1bb4d73c070b920736e1f
Giovanni Maggini <giovanni.maggini@gmail.com>
2013-07-18 16:34:14 +0200

    added role NAMES in Role

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,6 @@
 class Role < ActiveRecord::Base
+  NAMES = %w"global_admin curator"
+
   belongs_to :authorizable, :polymorphic => true
   belongs_to :user
   attr_accessible :name, :user, :authorizable
