f75adc6a8aa301902562cf11c5db49f66c464e8c
Piotr Steininger <piotr.steininger@gmail.com>
2013-07-24 20:50:21 +0200

    fixed url redirects, added show method back in

--- app/controllers/route_profiles_controller.rb
+++ app/controllers/route_profiles_controller.rb
@@ -22,7 +22,7 @@ class RouteProfilesController < InheritedResources::Base
   # POST /route_profiles.json
   def create
     create! {
-      params[:route_profile][:image].present? ? crop_route_profile_url(@route_profile) : route_profiles_url
+      params[:route_profile][:image].present? ? crop_route_profile_url(@route_profile) : route_profile_url(@route_profile)
     }
   end
 
@@ -31,7 +31,7 @@ class RouteProfilesController < InheritedResources::Base
 # PUT /route_profiles/1.json
   def update
     update! {
-      params[:route_profile][:image].present? ? crop_route_profile_url(@route_profile) : route_profiles_url
+      params[:route_profile][:image].present? ? crop_route_profile_url(@route_profile) : route_profile_url(@route_profile)
     }
   end
 
